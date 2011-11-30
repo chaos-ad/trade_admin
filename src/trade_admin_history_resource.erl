a55d2ca1be7d8ac7662237a1a6e511bd2733a2bb
Anatoly Kanivetsky <chaos-ad@yandex.ru>
2011-11-30 10:19:43 +0400

    Keep testing highstock functionality.
    Handle index.html in static_resource and mode arg in dynamic

--- src/trade_admin_history_resource.erl
+++ src/trade_admin_history_resource.erl
@@ -13,29 +13,41 @@ to_json(ReqData, State) ->
     Period = wrq:get_qs_value("period", ReqData),
     From   = wrq:get_qs_value("from", ReqData),
     To     = wrq:get_qs_value("to", ReqData),
-    {get_history(Symbol, Period, From, To), ReqData, State}.
+    Mode   = wrq:get_qs_value("mode", ReqData),
+    {get_history(Symbol, Period, Mode, From, To), ReqData, State}.
 
-get_history(Symbol, Period, From, undefined) ->
+get_history(Symbol, Period, Mode, From, undefined) ->
     T1 = edate:string_to_date(From),
-    jsonize( trade_history:get_history(Symbol, list_to_integer(Period), T1) );
+    Data = trade_history:get_history(Symbol, list_to_integer(Period), T1),
+    jsonize(Mode, Data);
 
-get_history(Symbol, Period, From, To) ->
+get_history(Symbol, Period, Mode, From, To) ->
     T1 = edate:string_to_date(From),
     T2 = edate:string_to_date(To),
-    jsonize( trade_history:get_history(Symbol, list_to_integer(Period), T1, T2) ).
+    Data = trade_history:get_history(Symbol, list_to_integer(Period), T1, T2),
+    jsonize(Mode, Data).
 
-jsonize(History) ->
-    "?(\n[" ++
-    string:join
-    (
-        lists:map
-        (
-            fun(Bar) ->
-                lists:flatten(io_lib:format("[~B000, ~p, ~p, ~p, ~p, ~p]", tuple_to_list(Bar)))
-            end,
-            History
-        ),
-        ",\n"
-    )
-    ++ "]);".
-%     "[]". %% TODO: Fixme
+jsonize(Mode, History) when is_list(History) ->
+    Format = make_format(Mode),
+    "[\n" ++ string:join([jsonize(Mode, Format, Bar) || Bar <- History], ",\n") ++ "\n]".
+
+jsonize(Mode, Format, Bar) when is_tuple(Bar) ->
+    lists:flatten(io_lib:format(Format, make_args(Mode, Bar))).
+
+make_format(Fmt) -> make_format(Fmt, []).
+make_format([], Acc) -> "[" ++ string:join(lists:reverse(Acc), ", ") ++ "]";
+make_format([$v|Tail], Acc) -> make_format(Tail, ["~B"|Acc]);
+make_format([$o|Tail], Acc) -> make_format(Tail, ["~.2f"|Acc]);
+make_format([$h|Tail], Acc) -> make_format(Tail, ["~.2f"|Acc]);
+make_format([$l|Tail], Acc) -> make_format(Tail, ["~.2f"|Acc]);
+make_format([$c|Tail], Acc) -> make_format(Tail, ["~.2f"|Acc]);
+make_format([$t|Tail], Acc) -> make_format(Tail, ["~B000"|Acc]).
+
+make_args(Fmt, Bar) -> make_args(Fmt, Bar, []).
+make_args([], _, Acc) -> lists:reverse(Acc);
+make_args([$t|Tail], Bar, Acc) -> make_args(Tail, Bar, [trade_utils:time(Bar)|Acc]);
+make_args([$o|Tail], Bar, Acc) -> make_args(Tail, Bar, [trade_utils:open(Bar)|Acc]);
+make_args([$h|Tail], Bar, Acc) -> make_args(Tail, Bar, [trade_utils:high(Bar)|Acc]);
+make_args([$l|Tail], Bar, Acc) -> make_args(Tail, Bar, [trade_utils:low(Bar)|Acc]);
+make_args([$c|Tail], Bar, Acc) -> make_args(Tail, Bar, [trade_utils:close(Bar)|Acc]);
+make_args([$v|Tail], Bar, Acc) -> make_args(Tail, Bar, [trade_utils:volume(Bar)|Acc]).
--- src/trade_admin_static_resource.erl
+++ src/trade_admin_static_resource.erl
@@ -1,41 +1,36 @@
 -module(trade_admin_static_resource).
--export([init/1,
-         content_types_provided/2,
-         resource_exists/2,
-         content/2]).
+-export([init/1, content_types_provided/2, resource_exists/2, provide_content/2]).
 
--record(context, {root, filepath}).
+-record(context, {docroot, filepath}).
 
 -include_lib("webmachine/include/webmachine.hrl").
 
 init(Opts) ->
     {ok, App} = application:get_application(),
     PrivDir = code:priv_dir(App),
-    RootDir = PrivDir ++ "/" ++ proplists:get_value(root, Opts, []),
-    {ok, #context{root=RootDir}}.
+    PathDir = proplists:get_value(root, Opts, []),
+    RootDir = filename:join([PrivDir, PathDir]),
+    {ok, #context{docroot=RootDir}}.
 
-content_types_provided(RD, Context) ->
-    Path = wrq:disp_path(RD),
-    Mime = webmachine_util:guess_mime(Path),
-    {[{Mime, content}], RD, Context}.
-
-resource_exists(RD, Context=#context{root=Root}) ->
-    FP = filename:join([Root|clean_path(wrq:disp_path(RD))]),
-    lager:debug("Static file request: ~p~n", [FP]),
-    case filelib:is_regular(FP) of
-        true ->
-            {true, RD, Context#context{filepath=FP}};
-        _ ->
-            {false, RD, Context}
+content_types_provided(ReqProps, Context) ->
+    case wrq:disp_path(ReqProps) of
+        ""   -> {[{"text/html", provide_content}], ReqProps, Context};
+        Path -> {[{webmachine_util:guess_mime(Path), provide_content}], ReqProps, Context}
     end.
 
-content(RD, Context=#context{filepath=FP}) ->
-    {ok, Data} = file:read_file(FP),
-    {Data, RD, Context}.
+resource_exists(ReqProps, Context=#context{docroot=DocRoot}) ->
+    case mochiweb_util:safe_relative_path(wrq:disp_path(ReqProps)) of
+        undefined -> {false, ReqProps, Context};
+        ""        -> {true,  ReqProps, Context#context{filepath=filename:join([DocRoot, "index.html"])}};
+        SafePath  ->
+            FilePath = filename:join([DocRoot, SafePath]),
+            case filelib:is_regular(FilePath) of
+                true  -> {true,  ReqProps, Context#context{filepath=FilePath}};
+                false -> {false, ReqProps, Context}
+            end
+    end.
 
-clean_path(Path) ->
-    Fun = fun("..", [])       -> [];     % no shallower
-             ("..", [_|Rest]) -> Rest;   % one shallower
-             (P,    Acc)      -> [P|Acc] % one deeper
-          end,
-    lists:reverse(lists:foldl(Fun, [], string:tokens(Path, "/"))).
+provide_content(ReqProps, Context=#context{filepath=FilePath}) ->
+%     lager:debug("Sending static file: ~p", [FilePath]),
+    {ok, Value} = file:read_file(FilePath),
+    {Value, ReqProps, Context}.
