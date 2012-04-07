55f396bad2996142b95a151b5955b9faaf02b63b
Anatoly Kanivetsky <Chaos.A.D88@gmail.com>
2012-04-08 00:39:01 +0400

    Securities gzipped and ETag-ged.

--- src/trade_admin_securities_resource.erl
+++ src/trade_admin_securities_resource.erl
@@ -1,19 +1,36 @@
 -module(trade_admin_securities_resource).
--export([init/1, content_types_provided/2, to_json/2]).
+
+-compile(export_all).
 
 -include_lib("webmachine/include/webmachine.hrl").
 
+-record(state, {securities, etag}).
+
 init([]) ->
-    {ok, undefined}.
+    {ok, #state{}}.
 
 content_types_provided(ReqData, State) ->
     {[{"application/json", to_json}], ReqData, State}.
 
-to_json(ReqData, State) ->
-    Data = lists:map(fun prepare/1, trade_db:get_all_symbols()),
-    {ok, JSON} = json:encode(Data),
+encodings_provided(ReqData, State) ->
+    {[{"identity", fun(X) -> X end}, {"gzip", fun zlib:gzip/1}], ReqData, State}.
+
+generate_etag(ReqData, State=#state{etag=undefined, securities=undefined}) ->
+    Securities = get_securities(wrq:path_info(market, ReqData)),
+    Hash = erlang:phash2(Securities),
+    ETag = mochihex:to_hex(Hash),
+    {ETag, ReqData, State#state{etag=ETag, securities=Securities}};
+
+generate_etag(ReqData, State=#state{etag=ETag}) ->
+    {ETag, ReqData, State}.
+
+to_json(ReqData, State=#state{securities=Securities}) when is_list(Securities) ->
+    {ok, JSON} = json:encode(lists:map(fun prepare/1, Securities)),
     {JSON, ReqData, State}.
 
-prepare({symbol, ID, Code, Name, Market}) ->
-    {[{"id", ID}, {code, Code}, {name, Name}, {market, Market}]}.
+get_securities("all") -> trade_db:get_all_symbols();
+get_securities(undefined) -> trade_db:get_all_symbols();
+get_securities(Market) when is_list(Market) -> trade_db:get_all_symbols(list_to_integer(Market)).
 
+prepare({symbol, ID, Code, Name, Market}) ->
+    {[{id, ID}, {code, Code}, {name, Name}, {market, Market}]}.
